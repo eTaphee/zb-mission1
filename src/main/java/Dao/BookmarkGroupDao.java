b712a345479f3ccb9d8649a2c135ea338738c5ff
thlee <e-tap@naver.com>
2023-09-25 20:40:09 +0900

    BookmarkGroup SQLite 연동
    - BookmarkGroup 데이터베이스 엔티티 모델
    - BookmarkGroup DAO(findById, findAll, create, update, delete)

---
+++ src/main/java/Dao/BookmarkGroupDao.java
@@ -0,0 +1,119 @@
+package Dao;
+
+import Entity.BookmarkGroup;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookmarkGroupDao extends BaseDao {
+    public BookmarkGroupDao() throws ClassNotFoundException {
+        super();
+    }
+
+    public BookmarkGroup findById(int id) {
+        try (
+                Connection conn = getConnection();
+                PreparedStatement statement = conn.prepareStatement("select id, name, [order], updated_dttm from bookmark_group where id = ?;");
+        ) {
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return BookmarkGroup.builder()
+                            .id(resultSet.getInt(1))
+                            .name(resultSet.getString(2))
+                            .order(resultSet.getInt(3))
+                            .updatedDateTime(LocalDateTime.parse(resultSet.getString(4)))
+                            .build();
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<BookmarkGroup> findAll() {
+        List<BookmarkGroup> bookmarkGroupList = new ArrayList<>();
+
+        try (
+                Connection conn = getConnection();
+                PreparedStatement statement = conn.prepareStatement("select id, name, [order], updated_dttm from bookmark_group order by [order] desc;");
+                ResultSet resultSet = statement.executeQuery();
+        ) {
+            while (resultSet.next()) {
+                bookmarkGroupList.add(BookmarkGroup
+                        .builder()
+                        .id(resultSet.getInt(1))
+                        .name(resultSet.getString(2))
+                        .order(resultSet.getInt(3))
+                        .updatedDateTime(LocalDateTime.parse(resultSet.getString(4)))
+                        .build());
+            }
+            return bookmarkGroupList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public BookmarkGroup create(BookmarkGroup bookmarkGroup) {
+        try (
+                Connection conn = getConnection();
+                PreparedStatement statement = conn.prepareStatement("insert into bookmark_group(name, [order], updated_dttm) values(?, ?, ?);");
+        ) {
+            LocalDateTime now = LocalDateTime.now();
+            statement.setString(1, bookmarkGroup.getName());
+            statement.setInt(2, bookmarkGroup.getOrder());
+            statement.setString(3, now.toString());
+            statement.executeUpdate();
+
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    return bookmarkGroup
+                            .toBuilder()
+                            .id(resultSet.getInt(1))
+                            .updatedDateTime(now)
+                            .build();
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean update(BookmarkGroup bookmarkGroup) {
+        try (
+                Connection conn = getConnection();
+                PreparedStatement statement = conn.prepareStatement("update bookmark_group set name = ?, [order] = ?, updated_dttm = ? where id = ?");
+        ) {
+            statement.setString(1, bookmarkGroup.getName());
+            statement.setInt(2, bookmarkGroup.getOrder());
+            statement.setString(3, LocalDateTime.now().toString());
+            statement.setInt(4, bookmarkGroup.getId());
+            return (statement.executeUpdate() > 0);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean delete(BookmarkGroup bookmarkGroup) {
+        try (
+                Connection conn = getConnection();
+                PreparedStatement statement = conn.prepareStatement("delete from bookmark_group where id = ?");
+        ) {
+            statement.setInt(1, bookmarkGroup.getId());
+            return (statement.executeUpdate() > 0);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+    }
+}
---
+++ src/main/java/Entity/BookmarkGroup.java
@@ -0,0 +1,26 @@
+package Entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder(toBuilder = true)
+@Getter
+public class BookmarkGroup {
+    private int id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private int order;
+
+    private LocalDateTime updatedDateTime;
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
